663d69517027136ce2b808c3fc7e00a6493294e5
lihongbin <lihongbin@cyai.com>
2019-05-23 13:33:29 +0800

    ----

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 #人生苦短，我用Python！！！
 print('人生苦短，我用python')
 import datetime
-print('今天是：', datetime.datetime.now())
+print('今天是：', datetime.datetime.now()
+
+print('今天第二次修改', datetime.datetime.now()))
